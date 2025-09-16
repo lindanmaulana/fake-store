928b6bae7497ac173db7f2cc0898b82025ed2000
Lindan Maulana <lindanstemga@gmail.com>
2025-09-16 14:25:13 +0700

    refactor: spacing link footer and text color

--- src/app/(home)/_components/footer.tsx
+++ src/app/(home)/_components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <footer>
             <div className="border-y border-black/10 py-10">
                 <div className="container max-w-6xl mx-auto px-8 lg:px-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-6">
-                    <div>
+                    <div className="space-y-3">
                         <div className="flex items-center gap-2">
                             <div className="block size-6 bg-gradient-to-br from-blue-700 to-purple-500 rounded-lg"></div>
                             <h2 className="text-xl font-bold">Fake Store</h2>
@@ -13,9 +13,9 @@ export const Footer = () => {
                         <p className="text-base text-gray-500">Jelajahi produk berdasarkan kategori</p>
                     </div>
 
-                    <div className="space-y-2">
+                    <div className="space-y-3">
                         <h3 className="font-semibold">Navigasi</h3>
-                        <ul className="text-sm text-gray-500">
+                        <ul className="text-sm text-gray-500 space-y-2">
                             <li>
                                 <Link href={"/products"}>Produk</Link>
                             </li>
@@ -25,14 +25,14 @@ export const Footer = () => {
                         </ul>
                     </div>
 
-                    <div className="space-y-2">
+                    <div className="space-y-3">
                         <h3 className="font-semibold">Sumber</h3>
                         <p className="text-sm text-gray-500">Data oleh <a href="" className="border-b-1 border-purple-500">Fake Store API.</a></p>
                     </div>
                 </div>
             </div>
             <div className="w-full flex items-center justify-center py-4">
-                <p className="text-sm text-gray-400">© 2025 Fakestore+ Landing. All rights reserved.</p>
+                <p className="text-sm text-gray-500">© 2025 Fakestore+ Landing. All rights reserved.</p>
             </div>
         </footer>
     )
